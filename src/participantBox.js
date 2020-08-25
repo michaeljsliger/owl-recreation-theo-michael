60c9eb0229faa945b6eb623dcf8087ec3632a771
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-25 15:13:53 -0400

    replaced buttons

--- src/participantBox.js
+++ src/participantBox.js
@@ -5,11 +5,22 @@ import Participant from './Participant'
 class ParticipantBox extends Component{
 
     render(){
+
         const participantArray = STORE.filter(el => el.inSession === true).map(el => {
             return (<li key={el.id}><Participant  avatar={el.avatar} name={el.name}/></li>)
         })
         return (
+        <div>
+            <div className="button-box">
+            <button type="button" className="chat">
+                Chat 
+            </button>
+            <button type ="button" className="participants">
+                Particpants
+            </button>
+           </div>
           <ul className={this.props.className}>{participantArray}</ul>
+        </div>
         )
     }
 }
