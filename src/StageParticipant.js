1b720cadc6f1c35d6516b1dc748a0b8d4139da64
Theo McCauley <TheoMakaurii@gmail.com>
2020-08-25 15:05:06 -0400

    particpant box and partipants

--- src/StageParticipant.js
+++ src/StageParticipant.js
@@ -9,12 +9,23 @@ class StageParticipant extends Component {
     // render template
     render() {
         return (
+        <div>
+        <div className="button-box">
+             <button type="button" className="chat">
+                 Chat 
+             </button>
+             <button type ="button" className="participants">
+                 Particpants
+             </button>
+         </div> 
+
             <div className="stage-contact" id={this.props.id}>
                 <div className="stage-contact-header">
                     {this.props.name}
                 </div>
                <img className="stage-contact-image" src={this.props.avatar} alt="huh" />
             </div>
+        </div>
         );
     }
 }
