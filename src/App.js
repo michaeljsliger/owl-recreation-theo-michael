78e41da90287f8334c8ee55dbc1e02cd54bea84a
Michael Sliger <michaelj.sliger@gmail.com>
2020-08-25 10:27:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+
+// three components, with other subcomponents
+// participant list component, participant component
+// stage list component (flex box)
+// stage individual component
+// chat log component, chat entry component
+// associated testing
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
